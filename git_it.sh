370648ddeb2a54fb0c0f447b2533795234420fc1
davidyuqiwei <davidyuqiwei@outlook.com>
2021-01-15 09:56:28 +0800

    david

--- git_it.sh
+++ git_it.sh
@@ -4,7 +4,7 @@
 folder_size=`du -s | awk '{ print $1  }'`
 foler_size_M=`du -sh`
 echo $folder_size_M
-if [ $folder_size -gt 35704 ]
+if [ $folder_size -gt 65704 ]
 then 
   echo 'Error: there has large files'
   exit 1
